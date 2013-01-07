4bfe72d2ed113c1e047eaac8d967cf92fd963e60
Lars Vogel <Lars.Vogel@gmail.com>
2013-01-07 15:53:58 +0100

    Rssfeed Reader - Different layouts for landscape and portrait

--- com.example.android.rssfeed/src/com/example/android/rssfeed/RssfeedActivity.java
+++ com.example.android.rssfeed/src/com/example/android/rssfeed/RssfeedActivity.java
@@ -1,22 +1,39 @@
 package com.example.android.rssfeed;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
-public class RssfeedActivity extends Activity {
+public class RssfeedActivity extends Activity implements
+		MyListFragment.OnItemSelectedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rssfeed);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rssfeed);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_rssfeed, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_rssfeed, menu);
+		return true;
+	}
+
+	@Override
+	public void onRssItemSelected(String link) {
+		DetailFragment fragment = (DetailFragment) getFragmentManager()
+				.findFragmentById(R.id.detailFragment);
+		if (fragment != null && fragment.isInLayout()) {
+			fragment.setText(link);
+		} else {
+			Intent intent = new Intent(getApplicationContext(),
+					DetailActivity.class);
+			intent.putExtra(DetailActivity.EXTRA_URL, link);
+			startActivity(intent);
+
+		}
+	}
+
 }
