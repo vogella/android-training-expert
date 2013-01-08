273baebbba8f0b655f36cde31eb12281d4618c71
sergej <beworker@gmail.com>
2013-01-08 20:16:23 +0100

    adding new styles example project

---
+++ de.vogella.styles/src/de/vogella/styles/WallpaperActivity.java
@@ -0,0 +1,14 @@
+package de.vogella.styles;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WallpaperActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.wallpaper_activity);
+	}
+
+}
